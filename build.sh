1b7e2b28d3586886e4690144ace33748b8b642f1
Pete Newnham <pete@blarg.co.uk>
2013-11-01 14:32:54 +0000

    Updated formatting options for build script

--- build.sh
+++ build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+ZIP=false
+if [ $# -eq 1 ] && [ "$1" = "zip" ]
+then
+    ZIP=true
+fi
+
 # remove existing build dir
 echo "removing build"
 rm -rf build/
@@ -20,15 +26,21 @@ rm -rf build/options
 
 # build js and remove src
 
+FORMATTING=""
+if [ $ZIP != true ]
+then
+    FORMATTING="--formatting pretty_print"
+fi
+
 echo "compiling content.js"
-find src/js/content/ -name "*.js" | xargs java -jar deps/compiler.jar --js_output_file build/content.js --compilation_level WHITESPACE_ONLY --js
+find src/js/content/ -name "*.js" | xargs java -jar deps/compiler.jar --js_output_file build/content.js --compilation_level WHITESPACE_ONLY $FORMATTING --js
 
 echo "compiling background.js"
-find src/js/background/ -name "*.js" | xargs java -jar deps/compiler.jar --js_output_file build/background.js --compilation_level WHITESPACE_ONLY --js
+find src/js/background/ -name "*.js" | xargs java -jar deps/compiler.jar --js_output_file build/background.js --compilation_level WHITESPACE_ONLY $FORMATTING --js
 
 rm -rf build/js
 
-if [ $# -eq 1 ] && [ "$1" = "zip" ]
+if [ $ZIP = true ]
 then
     echo "zipping package"
     zip -r pageruler.zip build
